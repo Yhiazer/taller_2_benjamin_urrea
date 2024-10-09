cf8e9914e1f5a10ca00d4bf3019f6c24f1d96770
Yhiazer <b.urrea01@ufromail.cl>
2024-10-09 15:42:41 -0300

    feat: Se añade componente para consumo de Api
    Se crea un componente para habilitar el consumo de api con un maquetado html simple

---
+++ src/components/ApiComponente.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Opciones</h1>
+        <div class="botones">
+            <button @click="buscarPoblacion">Buscar Población</button>
+            <button @click="buscarAnimeReco">Buscar Recomendaciones de Anime</button>
+            <button @click="buscarAnimeR">Ver Anime Aleatorio</button>
+            <button @click="buscarManga">Ver Manga Aleatorio</button>
+        </div>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
